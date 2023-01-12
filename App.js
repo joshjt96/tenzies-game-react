be5a6464db9df94994ed64e48532478b6966113e
Josh Talley <jtdev84@gmail.com>
2023-01-12 16:11:25 +0000

    Add files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    return (
+        <main>
+            <div className="main">
+            </div>
+        </main>
+    )
+}
+
+export default App
