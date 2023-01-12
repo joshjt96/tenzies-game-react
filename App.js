44eef8c7a71f6656eb79e0793de8eba140c20bc9
Josh Talley <jtdev84@gmail.com>
2023-01-12 16:48:31 +0000

    Partial second challenge

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import Die from "./components/Die.js"
 
-function App() {
+/**
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
+ */
+
+// Up to Grid section styling
+
+export default function App() {
     return (
         <main>
-            Main
-        </main>
+            <div>
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+                <Die value={6} />
+            </div>
+        </main>   
     )
 }
-
-export default App
