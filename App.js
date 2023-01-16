fc0785d3288abac3e6092985b972dcc3228cd693
Josh Talley <jtdev84@gmail.com>
2023-01-16 09:01:55 +0000

    Change Dice component name

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from "react"
-import Die from "./components/Die.js"
+import Dice from "./components/Dice.js"
 
 /**
  * Challenge:
  * 
- * - Create a Die component that takes a `value` prop
- * - Render 10 instances of the Die component (manually)
+ * - Create a Dice component that takes a `value` prop
+ * - Render 10 instances of the Dice component (manually)
  *      - Provide a number between 1-6 for the value on each
  *        for now
- * - Style the <main> and <Die> components 
+ * - Style the <main> and <Dice> components 
  *   to look like they do in the slide
  *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
+ *        of the Dice component, and use CSS Grid to lay them
  *        out evenly in 2 rows of 5 columns
  *      - Use flexbox on main to center the dice container
  *        in the center of the page
@@ -23,16 +23,16 @@ export default function App() {
     return (
         <main>
             <div>
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
-                <Die value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
+                <Dice value={6} />
             </div>
         </main>   
     )
